06b8d9b370f513eafe9e8ab01b266e078a07746b
Marc Lavallee <mlavallee@gmail.com>
2011-05-06 14:04:13 -0400

    Boilerplate for new WordPress plugin

---
+++ credit.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: Navis Media Credit
+ * Plugin URI: http://argoproject.org/blog/
+ * Description: Adds support for credit fields on media stored in WordPress
+ * Version: 0.1
+ * Author: Project Argo
+ * Author URI: http://argoproject.org/blog/
+ * License: GPLv2
+ * */
+?>
