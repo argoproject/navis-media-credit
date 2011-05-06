24cba0cacb8c7fc6ee24cc610940e1ee6d3b0268
Marc Lavallee <mlavallee@gmail.com>
2011-05-06 16:58:43 -0400

    Made TinyMCE-built HTML more closely resemble new user-facing code.

--- js/media_credit_editor_plugin.js
+++ js/media_credit_editor_plugin.js
@@ -26,33 +26,14 @@ tinymce.plugins.wpEditImage.prototype._do_shcode = function(co) {
             
             div_cls = (cls == 'aligncenter') ? 'mceTemp mceIEcenter' : 'mceTemp';
 
-            if ( cls == 'alignleft' ) {
-                cls = 'left';
-            }
-            else if ( cls == 'alignright' ) {
-                cls = 'right';
-            }
-
-/*
-            html = '<div id="' + id + '" class="module image ' + cls + ' ' + div_cls + '" draggable style="width: ' + parseInt( w ) + 'px;">' + c;
-            if ( cred ) {
-                html += '<p class="credit">' + cred + '</p>';
-            }
-            if ( cap ) {
-                html += '<p class="caption">' + cap + '</p>';
-            }
-            html += '</div>';
-            return html;
-*/
-
             //return '<div class="'+div_cls+'" draggable><dl id="'+id+'" class="wp-caption '+cls+'" style="width: '+(10+parseInt(w))+ 'px"><dt class="wp-caption-dt">'+c+'</dt><dd class="wp-caption-dd">'+cap+ '</dd><dd class="wp-caption-dd">'+cred +'</dd></dl></div>';
-            return '<div id="' + id + '" class="module image ' + cls + ' ' + div_cls + '" style="width: ' + parseInt( w ) + 'px;">' + c + '<p class="credit">' + cred + '</p><p class="caption">' + cap + '</p></div>';
+            return '<div id="' + id + '" class="module image ' + cls + ' ' + div_cls + '" style="width: ' + parseInt( w ) + 'px;">' + c + '<p class="wp-media-credit">' + cred + '</p><p class="wp-caption-text">' + cap + '</p></div>';
     });
 }
 
 tinymce.plugins.wpEditImage.prototype._get_shcode = function(co) {
     //return co.replace(/<div class="mceTemp[^"]*">\s*<dl([^>]+)>\s*<dt[^>]+>([\s\S]+?)<\/dt>\s*<dd[^>]+>(.+?)<\/dd>\s*<dd[^>]+>(.+?)<\/dd>\s*<\/dl>\s*<\/div>\s*/gi, function(a,b,c,cap,cred){
-    return co.replace(/<div([^>]+)>\s*(<(?:a|img).*?>)\s*<p class="credit">(.*?)<\/p>\s*<p class="caption">(.*?)<\/p><\/div>/gi, function(a,b,c,cred,cap) {
+    return co.replace(/<div([^>]+)>\s*(<(?:a|img).*?>)\s*<p class="wp-media-credit">(.*?)<\/p>\s*<p class="wp-caption-text">(.*?)<\/p><\/div>/gi, function(a,b,c,cred,cap) {
             var id, cls, w;
             
             id = b.match(/id=['"]([^'"]+)/i);
@@ -60,11 +41,11 @@ tinymce.plugins.wpEditImage.prototype._get_shcode = function(co) {
             w = c.match(/width=['"]([0-9]+)/);
 
             id = ( id && id[1] ) ? id[1] : '';
-            cls = ( cls && cls[1] ) ? cls[1] : 'right';
+            cls = ( cls && cls[1] ) ? cls[1] : 'alignright';
             w = ( w && w[1] ) ? w[1] : '';
 
             if ( ! w || ( ! cap && ! cred ) ) return c;
-            cls = cls.match(/left|right|center/) || 'right';
+            cls = cls.match(/left|right|center/) || 'alignright';
             cap = cap.replace(/<\S[^<>]*>/gi, '').replace(/'/g, '&#39;').replace(/"/g, '&quot;');
             cred = cred.replace(/<\S[^<>]*>/gi, '').replace(/'/g, '&#39;').replace(/"/g, '&quot;');
 
