29492d0595efbd1129cb8aada9637771c24f77f9
Wesley Lindamod <wesley.lindamood@gmail.com>
2011-11-08 13:05:32 -0500

    changing plugin and author URI

--- navis-media-credit.php
+++ navis-media-credit.php
@@ -1,11 +1,11 @@
 <?php
 /*
  * Plugin Name: Navis Media Credit
- * Plugin URI: http://argoproject.org/blog/
+ * Plugin URI: http://argoproject.org/
  * Description: Adds support for credit fields on media stored in WordPress
  * Version: 0.1
  * Author: Project Argo
- * Author URI: http://argoproject.org/blog/
+ * Author URI: http://argoproject.org/
  * License: GPLv2
  * */
 ?>
